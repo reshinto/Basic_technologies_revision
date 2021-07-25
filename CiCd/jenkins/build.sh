a8e834f134ba8372384165adbcbab3f110ecf864
Terence <35042213+reshinto@users.noreply.github.com>
2021-07-26 06:21:29 +0800

    Delete build.sh

--- CiCd/jenkins/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# chmod a+x ./build.sh
-# remove sudo if not required
-
-sudo docker pull jenkins/jenkins:lts-jdk11
-sudo docker run -d -u 0 -p 8080:8080 -p 50000:50000 -v ~/jenkins_home:/var/jenkins_home --name jenkins --restart unless-stopped jenkins/jenkins:lts-jdk11
-sudo docker exec jenkins cat var/jenkins_home/secrets/initialAdminPassword
