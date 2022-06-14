eb1d09acaf60c01c9a12de645e31571bc8ae70d6
Terence <35042213+reshinto@users.noreply.github.com>
2022-06-15 02:13:48 +0800

    Create ExecutionSchedulingDemo.java

---
+++ languageSpecific/java/demo/ExecutionSchedulingDemo.java
@@ -0,0 +1,39 @@
+/**
+ * Two threads chopping vegetables
+* uses java 11
+ * run command: javac ExecutionSchedulingDemo.java && java ExecutionSchedulingDemo
+ */
+
+class VegetableChopper extends Thread{
+
+    public int vegetable_count = 0;
+    public static boolean chopping = true;
+
+    public VegetableChopper(String name) {
+        this.setName(name);
+    }
+
+    public void run() {
+        while(chopping) {
+            System.out.println(this.getName() + " chopped a vegetable!");
+            vegetable_count++;
+        }
+    }
+}
+
+public class ExecutionSchedulingDemo {
+    public static void main(String args[]) throws InterruptedException {
+        VegetableChopper barron = new VegetableChopper("Barron");
+        VegetableChopper olivia = new VegetableChopper("Olivia");
+
+        barron.start();                     // Barron start chopping
+        olivia.start();                     // Olivia start chopping
+        Thread.sleep(1000);                 // continue chopping for 1 second
+        VegetableChopper.chopping = false;  // stop chopping
+
+        barron.join();
+        olivia.join();
+        System.out.format("Barron chopped %d vegetables.\n", barron.vegetable_count);
+        System.out.format("Olivia chopped %d vegetables.\n", olivia.vegetable_count);
+    }
+}
